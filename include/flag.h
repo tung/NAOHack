9d2b52eb78ae09f6b4f8d04b0cac7d56bf62d068
Tung Nguyen <tungtn3@gmail.com>
2014-07-08 01:32:25 +1000

    Patch: pilesize option (by Glyn Kennington)
    Ported from UnNetHack.
    
    Sets the maximum number of items that will show "Things that are here";
    larger piles will instead show "There are several/many things here."
    Minimum value is 1, which suppresses the "Things that are here" list.
    
    Preserves save compatibility.

--- include/flag.h
+++ include/flag.h
@@ -334,6 +334,7 @@ struct instance_flags {
 	boolean  showrace;	/* show hero glyph by race rather than by role */
 	boolean  travelcmd;	/* allow travel command */
 	int	 runmode;	/* update screen display during run moves */
+	int	 pilesize;	/* how many items to list automatically */
 #ifdef AUTOPICKUP_EXCEPTIONS
 	struct autopickup_exception *autopickup_exceptions[2];
 #define AP_LEAVE 0
